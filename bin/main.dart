5e669b834dfc845dcb7c4fa13638e7693786653b
Clément Alexandre <clmntlxndr@gmail.com>
2021-01-12 11:26:33 +0100

    Reliant

--- bin/main.dart
+++ bin/main.dart
@@ -3,24 +3,16 @@ import 'package:garage_le_retour/garage.dart';
 void main(List<String> arguments) {
   var voiture1 = Voiture();
   var voiture2 = Voiture();
-  var moto1 = Moto();
 
   voiture2.couleur = 'verte';
   var garage = Garage();
-  var cabinet = CabinetComptable();
-  cabinet.addClient(garage);
-
-  print(cabinet);
 
   print('Mon garage: $garage');
   garage.nouvellePriseCharge(voiture1);
   garage.nouvellePriseCharge(voiture2);
-  garage.nouvellePriseCharge(moto1);
   print('Mon garage: $garage');
 
   voiture1.demarrer();
-  moto1.mettreLaBequille();
-  print(cabinet);
 
   print('end');
 }
--- lib/garage.dart
+++ lib/garage.dart
@@ -1,56 +1,9 @@
-import 'voiture.dart';
-
-abstract class Observer {
-  // On recoit le changement d'un élément observé
-  void changed(Observable source, String event);
-}
-
-abstract class Observable {
-  // J'ai des abonnés
-  final observers = <Observer>[];
-}
-
-class CabinetComptable implements Observer {
-  final clients = <Object, int>{};
-
-  void addClient(Observable client) {
-    // Quand j'ai un nouveau client, je m'y abonne en tant qu'observateur
-    // et ainsi, je receverai ses changements
-    client.observers.add(this);
-    clients[client] = 0;
-  }
-
-  String toString() {
-    return "clients : $clients";
-  }
-
-  // Un élément auquel je suis abonné a changé.
-  // source = qui c'est ? event = nature du changement
-  void changed(Observable source, event) {
-    if (event == "client a payé") clients[source] += 15;
-  }
-}
-
-class Garage implements Observable {
-  // mes abonnés
-  final observers = <Observer>[];
-  final _objetsAReparer = <Vehicule>[];
+class Garage {
+  final _objetsAReparer = <Voiture>[];
   Garage();
 
-  void nouvellePriseCharge(Vehicule vehicule) {
+  void nouvellePriseCharge(Voiture vehicule) {
     _objetsAReparer.add(vehicule);
-
-    // j'envoie à mes abonnés une notification de mise à jour
-    notify("client a payé");
-  }
-
-  void notify(String event) {
-    // A tous mes abonnés oyé oyé
-    observers.forEach((observer) {
-      // je trigger votre méthode changed, en indiquant que je suis
-      // la source d'un changement, ainsi que l'évenement
-      observer.changed(this, event);
-    });
   }
 
   @override
@@ -59,13 +12,12 @@ class Garage implements Observable {
   }
 }
 
-class Voiture extends Vehicule {
+class Voiture {
   static const roues = 4;
   String couleur = 'blouge';
 
-  @override
   void demarrer() {
-    super.demarrer();
+    print('$hashCode ($runtimeType) vroum');
   }
 
   Voiture() {
@@ -74,22 +26,5 @@ class Voiture extends Vehicule {
   @override
   String toString() {
     return 'Voiture $couleur';
-  }
-}
-
-class Moto extends Vehicule {
-  static const roues = 2;
-  String couleur = 'bleue';
-
-  void mettreLaBequille() {
-    print("j'ai mis la béquille");
-  }
-
-  Moto() {
-    //  Coucou je suis le constructeur
-  }
-  @override
-  String toString() {
-    return 'Moto $couleur';
   }
 }
