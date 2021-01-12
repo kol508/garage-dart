ba1d769d1819891fc9f66c00b0aff3e85a95f855
Clément Alexandre <clmntlxndr@gmail.com>
2021-01-12 11:33:40 +0100

    Reliant bis

--- bin/main.dart
+++ bin/main.dart
@@ -1,11 +1,16 @@
 import 'package:garage_le_retour/garage.dart';
+import 'package:garage_le_retour/reliant.dart';
 
 void main(List<String> arguments) {
+  var garage = Garage();
   var voiture1 = Voiture();
   var voiture2 = Voiture();
 
+  var reliant = Reliant();
+
+  garage.nouvellePriseCharge(reliant);
+
   voiture2.couleur = 'verte';
-  var garage = Garage();
 
   print('Mon garage: $garage');
   garage.nouvellePriseCharge(voiture1);
--- lib/reliant.dart
+++ lib/reliant.dart
@@ -1,1 +1,8 @@
-class Reliant {}
+import 'package:garage_le_retour/garage.dart';
+
+abstract class TrucANePasModifier {}
+
+class Reliant extends TrucANePasModifier {
+  static const roues = 3;
+  Reliant();
+}
